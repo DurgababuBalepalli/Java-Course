6e04eaf707b2f6673002601d532696948f2b3f02
DurgababuBalepalli <bdurgababu2000@gmail.com>
2025-03-08 08:50:50 +0530

    spring basic project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@GetMapping("/welcome")
+	public String hello() {
+		return "welcome......";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
